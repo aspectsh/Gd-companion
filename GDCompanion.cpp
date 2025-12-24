aab6623c8ff454957d5aff9db4156fc41a27e602
aspectsh <yassoyasien@gmail.com>
2025-12-24 09:55:44 +0200

    GDCompanion.cpp

--- GDCompanion.cpp
+++ GDCompanion.cpp
@@ -4,12 +4,18 @@
 
 using namespace geode::prelude;
 
+const char* motivationalMessages[] = {
+    "Hey! Don't let that wave part get you down. Keep going!",
+    "Remember that even the best players started somewhere.",
+    "Geometry Dash is all about persistence. You got this!",
+    "Take a deep breath and give it another try!",
+    "Your skill is building up with every single attempt.",
+    "Small goals lead to big victories. Go!"
+};
+
 class $modify(MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) return false;
-
-        auto menu = this->getChildByID("bottom-menu");
-        if (!menu) return true;
 
         auto spr = CCSprite::createWithSpriteFrameName("GJ_optionsBtn_001.png");
         spr->setScale(0.8f);
@@ -18,29 +24,26 @@ class $modify(MenuLayer) {
             spr, this, menu_selector(MenuLayer::onOpenCompanion)
         );
 
-        menu->addChild(btn);
-        menu->updateLayout();
+        auto menu = this->getChildByID("bottom-menu");
+        if (menu) {
+            menu->addChild(btn);
+            menu->updateLayout(); 
+        }
 
         return true;
     }
 
     void onOpenCompanion(CCObject*) {
-        std::vector<std::string> messages = {
-            "Hey! Don't let that wave part get you down. Keep going!",
-            "Geometry Dash is all about persistence. You got this!",
-            "Take a deep breath and give it another try!",
-            "Small goals lead to big victories. Go!"
-        };
-
         std::random_device rd;
         std::mt19937 gen(rd());
-        std::uniform_int_distribution<> distrib(0, messages.size() - 1);
+        std::uniform_int_distribution<> distrib(0, 5);
+        
+        const char* message = motivationalMessages[distrib(gen)];
         
         FLAlertLayer::create(
             "Companion Tip",
-            messages[distrib(gen)],
+            message,
             "Close"
         )->show();
     }
 };
-
